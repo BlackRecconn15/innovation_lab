343c7a561e96b08fb306225c816d9daedd301a0e
Josue_Morales <100812147+BlackRecconn15@users.noreply.github.com>
2025-01-02 17:45:26 -0600

    Update api.js
    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000", // URL base de tu API
+    baseURL: "http://0.0.0.0:8000/", // URL base de tu API
 });
 
 // Agrega el token en cada solicitud
